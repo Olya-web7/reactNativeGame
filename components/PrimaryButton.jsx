5d320b61c7c8ac66a3f994ef6479ff61ce83c347
Olya <ola.shy@yahoo.com>
2022-07-03 22:10:59 +0200

    fix

--- components/PrimaryButton.jsx
+++ components/PrimaryButton.jsx
@@ -2,28 +2,42 @@ import { Text, View, Pressable, StyleSheet } from "react-native";
 
 export default function PrimaryButton({ children }) {
   function pressHandler() {
-    console.log('pressed');
+    console.log("pressed");
   }
   return (
-    <Pressable onPress={pressHandler}>
-      <View style={styles.container}>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{ color: "#640233" }}
+      >
         <Text style={styles.buttonText}>{children}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#72063c",
+  buttonOuterContainer: {
     borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
     paddingVertical: 8,
     paddingHorizontal: 16,
-    margin: 4,
     elevation: 2,
   },
   buttonText: {
-    color: 'white',
-    textAlign: 'center',
-  }
-})
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
